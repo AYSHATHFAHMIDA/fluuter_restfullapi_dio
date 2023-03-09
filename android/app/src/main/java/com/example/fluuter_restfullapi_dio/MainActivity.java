d6d5840be05c13f697e6b073fb2893a5469072eb
AYHSATHFAHMIDA <ayshafahmi26@gmail.com>
2023-03-09 12:34:38 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/fluuter_restfullapi_dio/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.fluuter_restfullapi_dio;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/views/home_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:lottie/lottie.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
